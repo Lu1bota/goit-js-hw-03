628ecda61f29460d40c59804ee76397ae6fb2470
Danylo Liubota <daniil.luibota@gmail.com>
2025-02-04 17:02:26 +0200

    hw3

---
+++ js/task-2.js
@@ -0,0 +1,20 @@
+'use strict';
+
+function makeArray(firstArray, secondArray, maxLength) {
+    const totalArray = firstArray.concat(secondArray);
+    for (let i = 0; i < totalArray.length; i++) {
+        if (totalArray.length >= maxLength) {
+            const result = totalArray.slice(0, maxLength);
+            return result;
+        }
+        
+    }
+    return result;
+}
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
